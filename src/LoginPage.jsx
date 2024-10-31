e1b80e5e696b43e4eb5b43c7b9bdcc3e0acebb60
Diego Costa <diego.costa@ciag.org.br>
2024-10-31 15:00:24 -0300

    feat: video

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,9 +1,10 @@
 // LoginPage.jsx
 import React from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
-import { Card, Container, Button } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function LoginPage() {
   const navigate = useNavigate();
@@ -21,7 +22,21 @@ export default function LoginPage() {
 
   return (
     <GoogleOAuthProvider clientId="523729213609-24gkt8pfpnu9eq9kmhri0otrp2u8jad8.apps.googleusercontent.com">
-      <Container className="d-flex justify-content-center align-items-center min-vh-100">
+      {/* Vídeo de fundo */}
+      <div className="video-background">
+        <video autoPlay muted loop className="video-foreground">
+          <source src="4990233-hd_1920_1080_30fps.mp4" type="video/mp4" /> {/* Substitua pelo caminho do seu vídeo */}
+          Seu navegador não suporta vídeos.
+        </video>
+      </div>
+      
+      <Container className="d-flex flex-column justify-content-center align-items-center min-vh-100">
+        {/* Imagem centralizada no topo */}
+        <img 
+          src="LOGOTIPO HORIZONTAL 1 - BRANCO.png" // Substitua pelo caminho da sua imagem
+          alt="Logo Euvatar"
+          style={{ maxWidth: '300px', marginBottom: '2rem' }} // Ajuste o tamanho conforme necessário
+        />
         <Card style={{ maxWidth: '400px', padding: '2rem', borderRadius: '15px' }} className="shadow-lg text-center">
           <Card.Body>
             <Card.Title style={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: '1.5rem' }}>
